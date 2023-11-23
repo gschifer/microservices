e11db5c2e598d1e1ce6ceec0be215328116aa07c
Gabriel Schiferdecke <gabriel.schifer@hotmail.com>
2023-11-22 21:03:40 -0300

    Fixed wrong directory

--- gateway-service/src/main/java/com/gateway/filter/LoggingFilter.java
+++ gateway-service/src/main/java/com/gateway/filter/LoggingFilter.java
@@ -1,4 +1,4 @@
-package com.gateway.Gateway.filter;
+package com.gateway.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- gateway-service/src/test/java/com/gateway/GatewayApplicationTests.java
+++ gateway-service/src/test/java/com/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gateway.Gateway;
+package com.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
